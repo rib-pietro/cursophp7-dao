b100c71935f01481e8d03374ab7a9dd31a9a8f48
Pietro Ribeiro <ribpietro@gmail.com>
2018-01-09 13:47:47 -0200

    Criada a função de lista

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <?php
 
 require_once("config.php");
-
+/*Carrega um usuário
 $root = new Usuario();
 
 $root->loadById(4);
 
 echo $root;
+*/
+
+/*Carrega uma lista de usuários
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+/*Carrega uma lista de usuários pelo login
+$search = Usuario::search("us");
+
+echo json_encode($search);
+*/
+
 /*
 $sql = new Sql();
 
@@ -14,4 +28,10 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */	
+
+//Carrega um usuário usando login e senha
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+
+echo $usuario;
 ?>
